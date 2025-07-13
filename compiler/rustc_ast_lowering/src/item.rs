acee350d009827083dbc912b11d1d9f0dc24cbd3
Dawid Lachowicz <domdaw7@gmail.com>
2025-07-13 14:44:24 +0100

    Lower contract declarations in proper context

--- compiler/rustc_ast_lowering/src/item.rs
+++ compiler/rustc_ast_lowering/src/item.rs
@@ -1223,12 +1223,6 @@ impl<'hir> LoweringContext<'_, 'hir> {
         contract: Option<&FnContract>,
         body: impl FnOnce(&mut Self) -> hir::Expr<'hir>,
     ) -> hir::BodyId {
-        let contract_decls = if let Some(contract) = contract {
-            Some(self.lower_stmts(&contract.declarations).0)
-        } else {
-            None
-        };
-
         self.lower_body(|this| {
             let params =
                 this.arena.alloc_from_iter(decl.inputs.iter().map(|x| this.lower_param(x)));
@@ -1241,6 +1235,8 @@ impl<'hir> LoweringContext<'_, 'hir> {
             //
             // { contract_requires(PRECOND); let __postcond = |ret_val| POSTCOND; postcond({ body }) }
             if let Some(contract) = contract {
+                let contract_decls = this.lower_stmts(&contract.declarations).0;
+
                 let precond = if let Some(req) = &contract.requires {
                     // Lower the precondition check intrinsic.
                     let lowered_req = this.lower_expr_mut(&req);
@@ -1299,18 +1295,13 @@ impl<'hir> LoweringContext<'_, 'hir> {
                 // Flatten the body into precond, then postcond, then wrapped body.
                 let wrapped_body = this.block_all(
                     body.span,
-                    match contract_decls {
-                        Some(decls) => this.arena.alloc_from_iter(
-                            decls
-                                .into_iter()
-                                .map(|d| Some(*d))
-                                .chain([precond, postcond].into_iter())
-                                .flatten(),
-                        ),
-                        None => {
-                            this.arena.alloc_from_iter([precond, postcond].into_iter().flatten())
-                        }
-                    },
+                    this.arena.alloc_from_iter(
+                        contract_decls
+                            .into_iter()
+                            .map(|d| Some(*d))
+                            .chain([precond, postcond].into_iter())
+                            .flatten(),
+                    ),
                     Some(body),
                 );
                 (params, this.expr_block(wrapped_body))
