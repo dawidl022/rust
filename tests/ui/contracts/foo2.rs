3bf81fa85974d5f97dcf67830fab818efafbb543
Dawid Lachowicz <domdaw7@gmail.com>
2025-07-14 12:36:42 +0100

    Enable contract-checks in UI test with contract variable declarations

--- tests/ui/contracts/foo2.rs
+++ tests/ui/contracts/foo2.rs
@@ -1,4 +1,5 @@
 //@ run-pass
+//@ compile-flags: -Zcontract-checks=yes
 #![feature(contracts)]
 //~^ WARN the feature `contracts` is incomplete and may not be safe to use and/or cause compiler crashes [incomplete_features]
 
