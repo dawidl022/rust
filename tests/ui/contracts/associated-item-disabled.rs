ce7f09fdc7234c595b82bcbfb9f535d102f8c609
Dawid Lachowicz <domdaw7@gmail.com>
2025-08-10 22:08:37 +0100

    Remove contract_checks intrinsic

--- tests/ui/contracts/associated-item-disabled.rs
+++ tests/ui/contracts/associated-item-disabled.rs
@@ -1,4 +1,5 @@
-// Ensure we don't ICE when lowering contracts on an associated item.
+// Ensure we don't ICE when contract present on an associated item but
+// contract-checks are disabled.
 
 //@ compile-flags: --crate-type=lib
 //@ check-pass
